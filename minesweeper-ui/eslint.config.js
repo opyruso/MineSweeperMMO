83f449d9fcb9fdfce82ccf607d6d55641f15d94f
oPyRuSo <opyruso@gmail.com>
2025-08-02 20:20:39 +0200

    chore(ui): drop vite react plugin

--- minesweeper-ui/eslint.config.js
+++ minesweeper-ui/eslint.config.js
@@ -1,7 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -10,8 +9,7 @@ export default defineConfig([
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      reactHooks.configs['recommended-latest']
     ],
     languageOptions: {
       ecmaVersion: 2020,
