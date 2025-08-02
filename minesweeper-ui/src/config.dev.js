42174f5cf0208ca4e2333ad812ae599921b8d130
opyru <opyru@oPy-2025.fritz.box>
2025-08-02 20:56:02 +0200

    clean

---
+++ minesweeper-ui/src/config.dev.js
@@ -0,0 +1,6 @@
+window.CONFIG = {
+  "clairobscur-api-url": "https://api-minesweeper-dev.opyruso.com",
+  "auth-url": "https://auth.opyruso.com",
+  "auth-realm": "development",
+  "auth-client-id": "minesweeper-client"
+};
---
+++ minesweeper-ui/src/config.rec.js
@@ -0,0 +1,6 @@
+window.CONFIG = {
+  "clairobscur-api-url": "https://api-minesweeper-rec.opyruso.com",
+  "auth-url": "https://auth.opyruso.com",
+  "auth-realm": "recette",
+  "auth-client-id": "minesweeper-client"
+};
