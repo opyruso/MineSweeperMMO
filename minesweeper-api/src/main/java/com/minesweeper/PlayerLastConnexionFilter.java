fa2e44d01a63844555d015bca56477dca778607e
oPyRuSo <opyruso@gmail.com>
2025-08-04 05:48:19 +0200

    fix(api): use keycloak id for player last connexion

--- minesweeper-api/src/main/java/com/minesweeper/PlayerLastConnexionFilter.java
+++ minesweeper-api/src/main/java/com/minesweeper/PlayerLastConnexionFilter.java
@@ -10,6 +10,7 @@ import jakarta.ws.rs.Priorities;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.ext.Provider;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -25,6 +26,9 @@ public class PlayerLastConnexionFilter implements ContainerRequestFilter {
     SecurityIdentity identity;
 
     @Inject
+    JsonWebToken jwt;
+
+    @Inject
     PlayerRepository playerRepository;
 
     @Override
@@ -34,13 +38,13 @@ public class PlayerLastConnexionFilter implements ContainerRequestFilter {
             return;
         }
 
-        String playerId = identity.getPrincipal().getName();
+        String playerId = jwt.getSubject();
         LocalDateTime now = LocalDateTime.now();
         Player player = playerRepository.findById(playerId);
         if (player == null) {
             player = new Player();
             player.setId(playerId);
-            player.setName(playerId);
+            player.setName(identity.getPrincipal().getName());
             player.setDateLastConnexion(now);
             playerRepository.persist(player);
         } else {
