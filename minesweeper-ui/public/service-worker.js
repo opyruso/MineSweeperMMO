d3cdc880a089bde928c41e5e7ba16dc5bafb744d
oPyRuSo <opyruso@gmail.com>
2025-08-06 05:48:12 +0200

    feat(pwa): improve service worker and pwa support

--- minesweeper-ui/public/service-worker.js
+++ minesweeper-ui/public/service-worker.js
@@ -1,12 +1,111 @@
-const CACHE_NAME = 'minesweeper-cache-v1';
-const URLS_TO_CACHE = [
+const CACHE_NAME = 'minesweeper-cache-v2';
+const ASSETS = [
   '/',
   '/index.html',
+  '/manifest.json',
+  '/config.js',
+  '/css/main.css',
+  '/js/App.js',
+  '/js/i18n.js',
+  '/js/index.js',
+  '/js/keycloak.js',
+  '/js/locales/en.js',
+  '/js/locales/fr.js',
+  '/js/pages/BoostPage.js',
+  '/js/pages/GamePage.js',
+  '/js/pages/GamesListPage.js',
+  '/js/pages/InfoPage.js',
+  '/js/pages/LeaderboardPage.js',
+  '/js/pages/LoginPage.js',
+  '/js/pages/SettingsPage.js',
+  '/images/icons/icon-1024.png',
+  '/images/icons/icon-192.png',
+  '/images/icons/icon-512.png',
+  '/images/icons/actions/icon_alarm.png',
+  '/images/icons/actions/icon_arrow_back.png',
+  '/images/icons/actions/icon_bomb_defused.png',
+  '/images/icons/actions/icon_bombs_found.png',
+  '/images/icons/actions/icon_boost.png',
+  '/images/icons/actions/icon_buy_large.png',
+  '/images/icons/actions/icon_buy_medium.png',
+  '/images/icons/actions/icon_buy_small.png',
+  '/images/icons/actions/icon_calendar.png',
+  '/images/icons/actions/icon_contracts.png',
+  '/images/icons/actions/icon_defuse_process.png',
+  '/images/icons/actions/icon_disconnect.png',
+  '/images/icons/actions/icon_empty_hole.png',
+  '/images/icons/actions/icon_explosion.png',
+  '/images/icons/actions/icon_eyes_close.png',
+  '/images/icons/actions/icon_eyes_open.png',
+  '/images/icons/actions/icon_map_size.png',
+  '/images/icons/actions/icon_medal.png',
+  '/images/icons/actions/icon_people.png',
+  '/images/icons/actions/icon_portfolio.png',
+  '/images/icons/actions/icon_refresh.png',
+  '/images/icons/actions/icon_scan_process.png',
+  '/images/icons/actions/icon_scanner_power.png',
+  '/images/icons/actions/icon_trophy_loser.png',
+  '/images/icons/actions/icon_trophy_top1.png',
+  '/images/icons/actions/icon_trophy_top2.png',
+  '/images/icons/actions/icon_trophy_top3.png',
+  '/images/icons/actions/icon_upgrade_income.png',
+  '/images/icons/actions/icon_upgrade_scanner.png',
+  '/images/icons/actions/icon_wheel.png',
+  '/images/icons/actions/icon_x.png',
+  '/images/icons/actions/icon_y.png',
+  '/images/images_background_boost.png',
+  '/images/images_background_game.png',
+  '/images/images_background_gameinprogress.png',
+  '/images/images_background_leaderboard.png',
+  '/images/images_background_login.png',
+  '/images/images_background_nogameinprogress.png',
+  '/images/images_background_parameters.png',
+  '/images/images_background_rotatemobile.png',
+  '/images/images_background_upgrade.png',
+  '/sounds/sound_background.mp3',
+  '/sounds/sound_click_1.mp3',
+  '/sounds/sound_click_2.mp3',
+  '/sounds/sound_explosion.mp3',
+  '/sounds/sound_nothing.mp3',
+  '/sounds/sound_warning.mp3',
+  '/vendor/react.development.js',
+  '/vendor/react-dom.development.js',
+  '/vendor/react-router.development.js',
+  '/vendor/react-router-dom.development.js',
+  '/vendor/router.umd.js',
+  '/vendor/keycloak.js',
+  '/vendor/babel.min.js',
+  '/vendor/fontawesome/css/all.min.css',
+  '/vendor/fontawesome/webfonts/fa-brands-400.woff2',
+  '/vendor/fontawesome/webfonts/fa-regular-400.woff2',
+  '/vendor/fontawesome/webfonts/fa-solid-900.woff2',
+  '/vendor/fontawesome/webfonts/fa-v4compatibility.woff2',
+  '/vendor/flag-icons/css/flag-icons.min.css',
+  '/vendor/mouse-memoirs/index.css',
+  '/vendor/mouse-memoirs/files/mouse-memoirs-latin-400-normal.woff2',
+  '/vendor/mouse-memoirs/files/mouse-memoirs-latin-ext-400-normal.woff2'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+      .then(() =>
+        self.clients.matchAll().then((clients) =>
+          clients.forEach((client) => client.postMessage('CACHE_COMPLETE'))
+        )
+      )
   );
 });
 
@@ -14,4 +113,19 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => response || fetch(event.request))
   );
+});
+
+self.addEventListener('push', (event) => {
+  const data = event.data ? event.data.json() : {};
+  const title = data.title || 'Mine Sweeper Crew';
+  const options = {
+    body: data.body || '',
+    icon: '/images/icons/icon-192.png',
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('/'));
 });
